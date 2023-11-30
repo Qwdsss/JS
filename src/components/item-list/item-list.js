b832895f1f6f44ad58dc9e14cfb3d2d149a5b400
Qwdsss <artemiy.bobrus@gmail.com>
2023-11-30 10:38:19 +0600

    I have added wrapped component for simplisity using hard customising components

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import {withData} from "../hoc-helpers";
-import SwapiService from "../../services/swapi-service";
 import './item-list.css';
-
 
 
 const ItemList = (props) => {
     const {data, onItemSelected, children: renderLabel} = props;
-
     const items = data.map((item) => {
         const {id} = item;
         const label = renderLabel(item);
-
         return (
             <li
                 className="list-group-item"
@@ -21,7 +16,6 @@ const ItemList = (props) => {
                 {label}
             </li>)
     });
-
     return (
         <ul className="item-list list-group">
             {items}
@@ -29,6 +23,4 @@ const ItemList = (props) => {
     );
 };
 
-const {getAllPeople} = new SwapiService();
-
-export default withData(ItemList, getAllPeople)
+export default ItemList;
